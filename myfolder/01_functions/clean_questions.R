4db39dcd5431cdac2475ac7a4b18be10395372e9
shaharlab <shaharlab@shaharlab.com>
2022-01-19 08:48:09 +0000

    try

--- myfolder/01_functions/clean_questions.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(tidylog)
-
-#questionnaires:sub_num,response
-clean_question=questions%>%select(subj,response)
-
-save(clean_question,file='myfolder/03_data/02_aggregated_data/question_agg.Rdata')
--- myfolder/01_functions/clean_screensize.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(tidylog)
-
-#screen_size:sub_num,screen_width,screen_height
-clean_screensize=screensize%>%select(subj,screen_width,screen_height)
-
-save(clean_screensize,file='myfolder/03_data/02_aggregated_data/screensize_agg.Rdata')
